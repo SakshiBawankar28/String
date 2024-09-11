cb73b325eef1072fe0bf543a7cafe31cd8ad08d2
SakshiBawankar28 <sakshibawabkar@gmail.com>
2024-09-12 00:04:05 +0530

    String-Programs

---
+++ String/src/com/programs/ConvertToLowercase.java
@@ -0,0 +1,36 @@
+// Java program that converts a given string to lowercase:
+
+package com.programs;
+
+import java.util.Scanner;
+
+public class ConvertToLowercase 
+{	
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string : ");
+		String s = sc.next();
+		convertToLowercase(s);
+		
+	}
+	
+	
+	public static void convertToLowercase(String s)
+	{
+		char []charArray  = s.toCharArray();
+		
+		for(int i=0; i<charArray.length; i++)
+		{
+			if(charArray[i] >= 'A'  &&  charArray[i] <= 'Z')
+			{
+				charArray[i] = (char)(charArray[i] + 32);
+			}
+		}
+		// Convert the character array back to a string and print the result
+        String result = new String(charArray);
+        System.out.println("Output: " + result);
+	}
+
+}
+
